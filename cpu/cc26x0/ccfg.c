3a6f3a611c4475ddb267b23e4085f1c7781d0c8d
Leon M. George <leon@georgemail.eu>
2015-12-05 00:16:38 +0100

    cc26x0: add ccfg.c

---
+++ cpu/cc26x0/ccfg.c
@@ -0,0 +1,41 @@
+/**
+ * @ingroup     cpu_cc26x0
+ * @{
+ *
+ * @file
+ * @brief       CCFG-definition
+ *
+ */
+
+#if UPDATE_CCFG
+/**
+ * @brief customer configuration (CCFG)
+ */
+__attribute__((section(".ccfg"), used))
+const uint32_t ccfg[] = {
+    0x01800000,         //O_EXT_LF_CLK
+    0x0082FF10,         //MODE_CONF_1
+    0x0058FFF9,         //SIZE_AND_DIS_FLAGS: XOSC_OVR disabled, ALT_DCDC enabled, GPRAM disabled ( | 4 to enable)
+    0xF39FFF3A,         //MODE_CONF: XOSC_LF (32.768kHz), XOSC_NO_CAP_MOD, XOSC_FREQ_24MHz_xtal, DCDC active/recharge
+    0xFFFFFFFF,         //VOLT_LOAD_0
+    0xFFFFFFFF,         //VOLT_LOAD_1
+    0xFFFFFFFF,         //RTC_OFFSET
+    0xFFFFFFFF,         //FREQ_OFFSET
+    0xFFFFFFFF,         //IEEE_MAC_0
+    0xFFFFFFFF,         //IEEE_MAC_1
+    0xFFFFFFFF,         //IEEE_BLE_0
+    0xFFFFFFFF,         //IEEE_BLE_1
+    0xC501FFFF,         //BL_CONFIG
+    0x00000101,         //ERASE_CONF
+    0x00000000,         //CCFG_TI_OPTIONS
+    0x00000000,         //CCFG_TAP_DAP_0
+    0x00000000,         //CCFG_TAP_DAP_1
+    0x00000000,         //IMAGE_VALID_CONF
+    0xFFFFFFFF,         //CCFG_PROT_31_0
+    0xFFFFFFFF,         //CCFG_PROT_63_32
+    0xFFFFFFFF,         //CCFG_PROT_95_64
+    0xFFFFFFFF          //CCFG_PROT_127_96
+};
+#endif
+
+/** @} */
