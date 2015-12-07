c153cddde67811376f99902a220c4f751665d71d
Leon M. George <leon@georgemail.eu>
2015-12-07 15:19:18 +0100

    cc26x0: add macros for UART register-fields

--- cpu/cc26x0/include/cc26x0-uart.h
+++ cpu/cc26x0/include/cc26x0-uart.h
@@ -42,7 +42,41 @@ typedef struct {
     reg32_t DMACTL;
 } UART_REGS_t;
 
-#define UART (*((UART_REGS_t *) (UART_BASE)))
+#define UART_DR_DATA_mask       0xFF
+#define UART_DR_FE              0x10
+#define UART_DR_PE              0x20
+#define UART_DR_BE              0x40
+#define UART_DR_OE              0x80
+
+#define UART_ECR_FE             0x1
+#define UART_ECR_PE             0x2
+#define UART_ECR_BE             0x4
+#define UART_ECR_OE             0x8
+
+#define UART_FR_CTS             0x1
+#define UART_FR_BUSY            0x4
+#define UART_FR_RXFE            0x10
+#define UART_FR_TXFF            0x20
+#define UART_FR_RXFF            0x40
+#define UART_FR_TXFE            0x80
+
+#define UART_LCHR_PEN           0x1
+#define UART_LCHR_EPS           0x2
+#define UART_LCHR_RXFE          0x4
+#define UART_LCHR_STP2          0x8
+#define UART_LCHR_FEN           0x10
+#define UART_LCHR_WLEN_mask     0x60
+#define UART_LCHR_WLEN          0x80
+
+#define UART_CTL_UARTEN         0x1
+#define UART_CTL_LBE            0x80
+#define UART_CTL_TXE            0x100
+#define UART_CTL_RXE            0x200
+#define UART_CTL_RTS            0x800
+#define UART_CTL_RTSEN          0x4000
+#define UART_CTL_CTSEN          0x8000
+
+#define UART ((UART_REGS_t *) (UART_BASE))
 
 #ifdef __cplusplus
 } /* end extern "C" */
