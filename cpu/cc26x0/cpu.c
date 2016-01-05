1daf5219289837ea4ebf57be1d964a73537a1308
Leon M. George <leon@georgemail.eu>
2016-01-05 01:08:09 +0100

    cc26x0: add comment containing potential future code

--- cpu/cc26x0/cpu.c
+++ cpu/cc26x0/cpu.c
@@ -9,6 +9,9 @@
  * @}
  */
 
+
+#include "cc26x0-prcm.h"
+
 #include "cpu.h"
 
 
@@ -19,4 +22,12 @@ void cpu_init(void)
 {
     /* initialize the Cortex-M core */
     cortexm_init();
+
+    /* turn on the AUX and JTAG power domains */
+    /* AON_WUC->AUXCTL = AUXCTL_AUX_FORCE_ON;
+    AON_WUC->JTAGCFG = JTAGCFG_JTAG_PD_FORCE_ON;
+
+    while (!(AON_WUC->PWRSTAT & PWRSTAT_AUX_PD_ON)) ;
+
+    DDI_0_OSC->CTL0 = DDI_0_OSC_CTL0_SCLK_LF_SRC_SEL_LF_RCOSC; */
 }
