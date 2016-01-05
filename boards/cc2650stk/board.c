e332fdef9a1803efb161dea55614d269df93288d
Leon M. George <leon@georgemail.eu>
2016-01-05 01:00:01 +0100

    cc2650stk: blink in board_init NOMERGE

--- boards/cc2650stk/board.c
+++ boards/cc2650stk/board.c
@@ -8,23 +8,41 @@
  * @author      Leon M. George <leon@georgemail.eu>
  */
 
-#include <stdio.h>
-
 #include "board.h"
 #include "cpu.h"
 
 #include "cc26x0-ioc.h"
 #include "cc26x0-gpio.h"
+#include "cc26x0-prcm.h"
 
 static void led_init(void)
 {
-    IOCFG_n(LED_RED_DIO) = IOCFG_QUICK_OUTPUT;
-    IOCFG_n(LED_GREEN_DIO) = IOCFG_QUICK_OUTPUT;
+    IOC->CFG[LED_RED_DIO] = 0;//IOCFG_QUICK_OUTPUT;
+    IOC->CFG[LED_GREEN_DIO] = 0;//IOCFG_QUICK_OUTPUT;
+    GPIO->DOE = ((1 << LED_RED_DIO) | (1 << LED_GREEN_DIO));
+
+    GPIO->DOUTSET = (1 << LED_RED_DIO);
+    GPIO->DOUTCLR = (1 << LED_GREEN_DIO);
+}
+
+static void _gpio_init(void)
+{
+    PRCM->PDCTL0 |= PDCTL0_PERIPH_ON;
+    while(!(PRCM->PDSTAT0 & PDSTAT0_PERIPH_ON)) ;
+
+    PRCM->GPIOCLKGR |= 1;
+    PRCM->CLKLOADCTL |= CLKLOADCTL_LOAD;
+    while (!(PRCM->CLKLOADCTL & CLKLOADCTL_LOADDONE)) ;
 }
 
 static void sleep(uint32_t s)
 {
-
+    while (s-- > 0) {
+        __asm__ volatile (
+            "nop\n\t"
+            "nop\n\t"
+            "nop\n\t"    );
+    }
 }
 
 /**
@@ -35,13 +53,14 @@ void board_init(void)
     /* initialize the CPU */
     cpu_init();
 
+    _gpio_init();
+
     /* initialize the boards LEDs */
     led_init();
 
-
     while (true) {
-        LED_GREEN_TOGGLE;
-        sleep(10);
+        GPIO->DOUTTGL = (1 << LED_RED_DIO) | (1 << LED_GREEN_DIO);
+        sleep(1000000);
     }
 }
 
