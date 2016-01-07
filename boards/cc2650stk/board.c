59a2df2c7ddf3b4c867af2d30597e2f84e5ec40f
Leon M. George <leon@georgemail.eu>
2016-01-08 00:50:17 +0100

    cc2650stk: remove unnecessary stuff from board_init

--- boards/cc2650stk/board.c
+++ boards/cc2650stk/board.c
@@ -23,47 +23,11 @@ static void led_init(void)
     gpio_init(LED_GREEN_DIO, GPIO_DIR_OUT, GPIO_NOPULL);
 }
 
-static void sleep(uint32_t s)
-{
-    while (s-- > 0) {
-        __asm__ volatile (
-            "nop\n\t"
-            "nop\n\t"
-            "nop\n\t"    );
-    }
-}
-
-static void rx_cb(void *arg, char data)
-{
-    GPIO->DOUTSET = (1 << LED_GREEN_DIO);
-    GPIO->DOUTTGL = (1 << LED_RED_DIO);
-}
-
-/**
- * @brief Initialize the SmartRF06 board
- */
 void board_init(void)
 {
-    /* initialize the CPU */
     cpu_init();
 
-    /* initialize the boards LEDs */
     led_init();
-
-    if (uart_init(0, 115200, rx_cb, NULL)) {
-        GPIO->DOUTTGL = (1 << LED_RED_DIO) | (1 << LED_GREEN_DIO);
-        sleep(1000000);
-        GPIO->DOUTTGL = (1 << LED_RED_DIO) | (1 << LED_GREEN_DIO);
-        sleep(1000000);
-        GPIO->DOUTTGL = (1 << LED_RED_DIO) | (1 << LED_GREEN_DIO);
-        sleep(1000000);
-        GPIO->DOUTTGL = (1 << LED_RED_DIO) | (1 << LED_GREEN_DIO);
-    }
-
-    while (true) {
-        uart_write(0, (unsigned char *) "hallo\r\n", 7);
-        sleep(1000000);
-    }
 }
 
 /** @} */
