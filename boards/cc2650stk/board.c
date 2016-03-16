8894aee8a6a086f8d35eaa5c91f80a59bb4286fe
Hauke Petersen <hauke.petersen@fu-berlin.de>
2016-03-16 19:42:08 +0100

    squash: various fixes to board code

--- boards/cc2650stk/board.c
+++ boards/cc2650stk/board.c
@@ -1,9 +1,9 @@
 /*
  * Copyright (C) 2016 Leon George
  *
- * This file is subject to the terms and conditions of the GNU Lesser General
- * Public License v2.1. See the file LICENSE in the top level directory for more
- * details.
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
  */
 /**
  * @ingroup     boards_cc2650stk
@@ -13,19 +13,17 @@
  * @brief       Board specific implementations for the CC2650STK board
  *
  * @author      Leon M. George <leon@georgemail.eu>
+ *
+ * @}
  */
 
+#include "cpu.h"
 #include "board.h"
-#include "cpu_conf.h"
-
-#include "periph/uart.h"
 
 void board_init(void)
 {
     cpu_init();
 
-    gpio_init(LED_RED_DIO, GPIO_DIR_OUT, GPIO_NOPULL);
-    gpio_init(LED_GREEN_DIO, GPIO_DIR_OUT, GPIO_NOPULL);
+    gpio_init(LED0_PIN, GPIO_DIR_OUT, GPIO_NOPULL);
+    gpio_init(LED1_PIN, GPIO_DIR_OUT, GPIO_NOPULL);
 }
-
-/** @} */
